fd13facca63720173f3e1f4c540c9c4abf08853f
jll9qw <lloyd804@gmail.com>
2019-06-24 20:54:18 -0400

    FINALLY! connected

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
+// Dependencies
+const express = require("express");
 const mysql = require("mysql");
-
-
-var app = express();
+const exphbs = require("express-handlebars");
+const app = express();
+const PORT = process.env.PORT || 3030;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,15 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
 
-app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
