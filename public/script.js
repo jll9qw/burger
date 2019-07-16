23d3f064513839d25b1c77edb03c690c3a8322f1
jll9qw <lloyd804@gmail.com>
2019-07-16 07:45:37 -0400

    updated script.js to on doc

--- public/script.js
+++ public/script.js
@@ -1,9 +1,5 @@
-
 $(document).ready(function() {
-  
     $(".change-devour").on("click", function(event) {
-      event.preventDefault();
-    
       var id = $(this).data("id");
       var newDevour = $(this).data("newdevour");
   
@@ -15,11 +11,28 @@ $(document).ready(function() {
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newDevourState
-      }).then(function() {
-        console.log("changed devour to", newDevour);
-        // Reload the page to get the updated list
-        location.reload();      });
+      }).then(
+        function() {
+          console.log("changed devour to", newDevour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
     
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE",
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
     });
   });
-  
